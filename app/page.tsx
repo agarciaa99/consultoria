2e4d8da292fa037f8cc27fea0195a3349d975195
agarciaa99 <aega2399@outlook.com>
2026-01-28 13:44:26 -0600

    feature: Configuracion de correo, validación, types y templates

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@/components/landing/header";
 import { Hero } from "@/components/landing/hero";
 import { Team } from "@/components/landing/team";
+import { Contact } from "@/components/landing/contact";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Hero />
       <Team />
+      <Contact />
     </main>
   );
 }
